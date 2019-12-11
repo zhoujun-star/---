d883d84815359fa88d8b3cf8d9e011483e5455e8
周俊 <52944518+zhoujun-star@users.noreply.github.com>
2019-12-11 17:57:59 +0800

    Add files via upload

--- admin.py
+++ admin.py
@@ -2,8 +2,10 @@ from tkinter import *
 from tkinter import ttk
 from sqlite_book import sql_book
 from sqlite_user import sql_user
+
+
 class admin_system(object):
-    def __init__(self,master,name):
+    def __init__(self, master, name):
         self.root = master
         self.root.geometry("800x600")
         canvas = Canvas(self.root)
@@ -16,43 +18,122 @@ class admin_system(object):
         self.Frm_Book.pack(expand=YES, fill=BOTH)
         frm_2 = Frame(self.Frm_Book)
         frm_2.pack(side=LEFT,fill = Y,padx= 10)
-        Entry(frm_2).pack(fill=X,pady=10)
+        self.varSearchBook = StringVar()
+        entry=Entry(frm_2, textvariable=self.varSearchBook)
+        entry.pack(fill=X,pady=10)
+        button_search = Button(entry, relief=GROOVE, text="搜索", command=self.search_book)
+        button_search.pack(side="right")
         type = ["经典著作","社会科学","军事科学","计算机技术","生活休闲","期刊","文学教育","音乐","摄影影视"]
-        self.tree = ttk.Treeview(frm_2)
+        self.tree = ttk.Treeview(frm_2, show="tree")
         for type_ in type:
             self.tree.insert("", 0, type_, text=type_)
-        self.tree.pack(side = LEFT, expand=YES,fill = Y)
+        book = sql_book()
+        self.book_classified_dictionary = book.QueryAll()
+        book.close()
+        for key in self.book_classified_dictionary:
+            for book in self.book_classified_dictionary[key]:
+                self.tree.insert(key, 0, book,text=book,)
+
+        # 分类框滚动条
+            # y
+        yscrollbar = Scrollbar(frm_2, orient=VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscrollcommand=yscrollbar.set)
+        yscrollbar.pack(side=RIGHT, fill=Y)
+            # x
+        xscrollbar = Scrollbar(frm_2, orient=HORIZONTAL, command=self.tree.xview)
+        self.tree.configure(xscrollcommand=xscrollbar.set)
+        xscrollbar.pack(side=BOTTOM, fill=X)
+
+        self.tree.pack(side=LEFT, expand=YES, fill=Y)
+        def click(event):
+            book = sql_book()
+            iidy = self.tree.identify_row(event.y)
+            if iidy in self.book_classified_dictionary:
+                pass
+            else:
+                selected_book = book.QueryByBookName(iidy)
+                self.var1.set(selected_book[0])
+                self.var2.set(selected_book[1])
+                self.var3.set(selected_book[2])
+                self.var4.set(selected_book[3])
+                self.var5.set(selected_book[4])
+                self.var6.set(selected_book[5])
+            book.close()
+
+
+        self.tree.bind("<Button-1>", click)
 
         frm_3 = Frame(self.Frm_Book)
         frm_3.pack(expand=YES)
         frm_31 = Frame(frm_3)
-        frm_31.pack(side=LEFT,expand=YES,fill=Y)
+        frm_31.pack(side=LEFT,fill=Y)
         Label(frm_31, text="书   名：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=10)
         Label(frm_31, text="作   者：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=10)
         Label(frm_31, text="出 版 社：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=10)
         Label(frm_31, text="ISBN：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=5)
         Label(frm_31, text="库   存：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=10)
         Label(frm_31, text="已   借：",width=8,font=('Arial',20),justify=LEFT).pack(side=TOP, padx=10,pady=10)
+        # 输入框框架
         frm_32 = Frame(frm_3)
-        frm_32.pack(side=LEFT,expand=YES,fill=Y)
-        var1 = StringVar(value="大数据挖掘与计算")
-        Entry(frm_32,textvariable = var1,state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
-        var2 = StringVar(value="李白")
-        Entry(frm_32, textvariable=var2, state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
-        var3 = StringVar(value="重庆第二师范学院")
-        Entry(frm_32, textvariable=var3, state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
-        var4 = StringVar(value="123456789")
-        Entry(frm_32, textvariable=var4, state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
-        var5 = StringVar(value="99")
-        Entry(frm_32, textvariable=var5, state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
-        var6 = StringVar(value="0")
-        Entry(frm_32, textvariable=var6, state=DISABLED,font=('Arial',20)).pack(side=TOP, padx=20,pady=10)
+        frm_32.pack(side=LEFT,fill=Y, expand=YES)
+        # 编辑框架
+        frm_33 = Frame(frm_32)
+        frm_33.pack(side="right", fill=Y, expand=YES)
+        # 结果显示区输入框
+        self.var1 = StringVar(value="大数据挖掘与计算")
+        self.et1 = Entry(frm_32,textvariable = self.var1,font=('Arial',20), state=DISABLED)
+        self.et1.pack(side=TOP, padx=10,pady=10)
+        self.var2 = StringVar(value="李白")
+        self.et2 = Entry(frm_32, textvariable=self.var2,font=('Arial',20), state=DISABLED)
+        self.et2.pack(side=TOP, padx=10, pady=10)
+        self.var3 = StringVar(value="重庆第二师范学院")
+        self.et3 = Entry(frm_32, textvariable=self.var3,font=('Arial',20), state=DISABLED)
+        self.et3.pack(side=TOP, padx=10, pady=10)
+        self.var4 = StringVar(value="123456789")
+        self.et4 = Entry(frm_32, textvariable=self.var4, font=('Arial',20), state=DISABLED)
+        self.et4.pack(side=TOP, padx=10, pady=10)
+        self.var5 = StringVar(value="99")
+        self.et5 = Entry(frm_32, textvariable=self.var5, font=('Arial',20), state=DISABLED)
+        self.et5.pack(side=TOP, padx=10, pady=10)
+        self.var6 = StringVar(value="0")
+        self.et6 = Entry(frm_32, textvariable=self.var6, font=('Arial',20), state=DISABLED)
+        self.et6.pack(side=TOP, padx=10, pady=10)
+        # 编辑区
+
+        self.lb1 = Label(frm_33, text="编辑")
+        self.lb1.pack(side="top", padx=20, pady=17)
+        self.lb1.bind("<Button-1>", lambda x: self.bindOfEdit(self, 1))
+        #self.lb1.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 1))
+        self.lb2 = Label(frm_33, text="编辑")
+        self.lb2.pack(side="top", padx=20,pady=16)
+        self.lb2.bind("<Button-1>", lambda x: self.bindOfEdit(self, 2))
+        #self.lb2.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 2))
+        self.lb3 = Label(frm_33, text="编辑")
+        self.lb3.pack(side="top", padx=20,pady=16)
+        self.lb3.bind("<Button-1>", lambda x: self.bindOfEdit(self, 3))
+        #self.lb3.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 3))
+        self.lb4 = Label(frm_33, text="编辑")
+        self.lb4.pack(side="top", padx=20,pady=16)
+        self.lb4.bind("<Button-1>", lambda x: self.bindOfEdit(self, 4))
+        #self.lb4.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 4))
+        self.lb5 = Label(frm_33, text="编辑")
+        self.lb5.pack(side="top", padx=20,pady=16)
+        self.lb5.bind("<Button-1>", lambda x: self.bindOfEdit(self, 5))
+        #self.lb5.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 5))
+        self.lb6 = Label(frm_33, text="编辑")
+        self.lb6.pack(side="top", padx=20, pady=16)
+        self.lb6.bind("<Button-1>", lambda x: self.bindOfEdit(self, 6))
+        #self.lb6.bind("<Enter>", lambda x: self.bindOfEditEnterColor(self, 6))
         frm_33 = Frame(self.Frm_Book)
         frm_33.pack(expand=YES)
         Button(frm_33, text="添加图书", relief=GROOVE,font=('Arial',20)).pack(side=LEFT,padx=20)
         Button(frm_33, text="修改图书", relief=GROOVE,font=('Arial',20)).pack(side=LEFT,padx=20)
         Button(frm_33, text="删除图书", relief=GROOVE,font=('Arial',20)).pack(side=LEFT,padx=20)
+##
+        self.lbs = [self.lb1, self.lb2, self.lb3, self.lb4, self.lb5, self.lb6]
+        self.ets = [self.et1, self.et2, self.et3, self.et4, self.et5, self.et6]
 
+        # 用户操作界面
         self.Frm_User = Frame(self.root)
         frm_4 = Frame(self.Frm_User)
         frm_4.pack(side=LEFT,fill=Y, padx=10)
@@ -82,21 +163,72 @@ class admin_system(object):
         Entry(frm_52, textvariable=var5, state=DISABLED, font=('Arial', 20)).pack(side=TOP, padx=20, pady=10)
         var6 = StringVar(value="0")
         Entry(frm_52, textvariable=var6, state=DISABLED, font=('Arial', 20)).pack(side=TOP, padx=20, pady=10)
-        frm_53= Frame(self.Frm_User)
+        frm_53 = Frame(self.Frm_User)
         frm_53.pack(expand=YES)
         Button(frm_53, text="添加用户", relief=GROOVE, font=('Arial', 20)).pack(side=LEFT, padx=10)
         Button(frm_53, text="更新用户", relief=GROOVE, font=('Arial', 20)).pack(side=LEFT, padx=10)
         Button(frm_53, text="删除用户", relief=GROOVE, font=('Arial', 20)).pack(side=LEFT, padx=10)
         Button(frm_53, text="借阅记录", relief=GROOVE, font=('Arial', 20)).pack(side=LEFT, padx=10)
+    def search_book(self):
+        for key in self.book_classified_dictionary:
+            if self.varSearchBook.get() in self.book_classified_dictionary[key]:
+
+                book = sql_book()
+                bookName = self.varSearchBook.get()
+                selected_book = book.QueryByBookName(bookName)
+                book.close()
+                self.var1.set(selected_book[0])
+                self.var2.set(selected_book[1])
+                self.var3.set(selected_book[2])
+                self.var4.set(selected_book[3])
+                self.var5.set(selected_book[4])
+                self.var6.set(selected_book[5])
+                break
+            else:
+                self.var1.set("无")
+                self.var2.set("无")
+                self.var3.set("无")
+                self.var4.set("无")
+                self.var5.set("无")
+                self.var6.set("无")
+
+    def bindOfEdit(event, self, number):
+
+        i = 0
+        for et in self.ets:
+            if i == number-1:
+                et["state"] = "normal"
+            else:
+                et["state"] = "disabled"
+            i += 1
+
+    # def bindOfEditEnterColor(event, self, number):
+    #     i=0
+    #     for lb in self.lbs:
+    #         if i == number-1:
+    #             lb["fg"] = "red"
+    #         else:
+    #             lb["fg"] = "black"
+    #         i += 1
+
+    def addBook(self):
+        pass
+    def delete(self):
+        pass
+    def alterBook(self):
+        pass
     def create_windows1(self):
         self.Frm_Book.pack_forget()
         self.Frm_User.pack_forget()
         self.Frm_Book.pack(expand=YES, fill=BOTH)
+
     def create_windows2(self):
         self.Frm_Book.pack_forget()
         self.Frm_User.pack_forget()
         self.Frm_User.pack(expand=YES, fill=BOTH)
+
+
 if __name__ == '__main__':
     root = Tk()
-    start = admin_system(root)
+    start = admin_system(root,"kljdk")
     root.mainloop()
--- sqlite_book.py
+++ sqlite_book.py
@@ -1,8 +1,11 @@
 import sqlite3
+
+
 class sql_book():
     def __init__(self):
         self.con = sqlite3.connect("Book.db")
         self.cur = self.con.cursor()
+
     def CreateTable(self):
         self.cur.execute("DROP TABLE IF EXISTS BOOKINFO")
         sql = """CREATE TABLE BOOKINFO(
@@ -16,11 +19,28 @@ class sql_book():
                 ROWID integer PRIMARY KEY autoincrement)"""
         self.cur.execute(sql)
         sqlist = ['''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
-                    VALUES("数据挖掘与机器学习","徐桂秋","人民邮电出版社","978-7-115-50345-2","100","100","YES")''',
+                    VALUES("数据挖掘与机器学习","徐桂秋","人民邮电出版社","978-7-115-50345-2","100","100","计算机技术")''',
                   '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
-                    VALUES("python编程基础与应用","林子雨","电子工业出版社","978-7-115-50345-3","100","100","YES")''',
+                    VALUES("python编程基础与应用","林子雨","电子工业出版社","978-7-115-50345-3","100","100","计算机技术")''',
                   '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
-                              VALUES("数据挖掘","徐桂秋","人民邮电出版社","978-7-115-50345-4","100","100","YES")''']
+                    VALUES("数据挖掘","徐桂秋","人民邮电出版社","978-7-115-50345-4","100","100","计算机技术")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("大学物理","渊小春","同济大学出版社","978-7-115-50345-5","100","100","社会科学")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("恶之花","[法]波德莱尔","江西人民出版社","978-7-115-50345-6","100","100","文学教育")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("摄影","李安强,郑强","清华大学出版社","978-7-115-50345-7","100","100","摄影影视")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("音乐教育论","郭声键","湖南文艺出版社","978-7-115-50345-8","100","100","音乐")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("藏馆 创刊号","邱双炯","藏馆编辑部","978-7-115-50345-9","100","100","期刊")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("失去的胜利","冯.埃里希.曼施泰因","民主与建设出版社","978-7-115-50345-10","100","100","军事科学")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("钢铁是怎样炼成的","尼古拉.奥斯特洛夫斯基","文化发展出版社","978-7-115-50345-11","100","100","经典著作")''',
+                  '''INSERT INTO BOOKINFO(BOOKNAME,AUTHOR,PUBLISH,ISBN,INVENTORY,CANBORROW,TYPE)
+                    VALUES("育儿百科","松田道雄","华夏出版社","978-7-115-50345-12","100","100","生活休闲")'''
+                  ]
         try:
             for sql_ in sqlist:
                 self.cur.execute(sql_)
@@ -56,6 +76,29 @@ class sql_book():
             print("数据查询失败:", e)
         finally:
             self.con.commit()
+
+    def QueryAll(self):
+        sql = f"""SELECT * FROM BOOKINFO;
+        """
+        book_dictionary = {"经典著作":[], "社会科学":[],"军事科学":[],"计算机技术":[],"生活休闲":[],"期刊":[],"文学教育":[],
+                           "音乐":[], "摄影影视":[]}
+        self.cur.execute(sql)
+        cp_book_dictionary = book_dictionary
+        for row in self.cur.fetchall():
+            print(row)
+            for key in book_dictionary.keys():
+                if row[-2] == key:
+                    cp_book_dictionary[key].append(row[0])
+                    break
+
+        print(cp_book_dictionary)
+        return cp_book_dictionary
+    def QueryByBookName(self, name):
+        sql = f"""SELECT * FROM BOOKINFO WHERE BOOKNAME LIKE '%{name}%'"""
+
+        self.cur.execute(sql)
+        return self.cur.fetchone()
+
     def Update(self):
         pass
     def Delete(self):
